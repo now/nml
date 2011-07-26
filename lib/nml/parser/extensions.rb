f32b575a3f3ebe1f64ca06b4acf86060d1e9767a
Nikolai Weibull <now@bitwi.se>
2011-07-26 18:47:38 +0200

    Improve parse error message slightly

--- lib/nml/parser/extensions.rb
+++ lib/nml/parser/extensions.rb
@@ -4,12 +4,13 @@ module NML::Parser::Extensions
   def ast(string)
     parser = new
     tree = parser.parse(string) or
-      raise NML::Parser::Error.new(parser.terminal_failures.count == 1 ?
-                                   parser.terminal_failures.first.expected_string.inspect :
-                                   'one of %s' % [parser.terminal_failures.
-                                                    map{ |f| f.expected_string.inspect }.
-                                                    uniq.
-                                                    join("\n")],
+      raise NML::Parser::Error.new('expected %s' %
+                                   (parser.terminal_failures.count == 1 ?
+                                      parser.terminal_failures.first.expected_string.inspect :
+                                      'one of %s' % [parser.terminal_failures.
+                                                     map{ |f| f.expected_string.inspect }.
+                                                     uniq.
+                                                     join("\n")]),
                                    parser.failure_line,
                                    parser.failure_column)
     tree.to_ast
