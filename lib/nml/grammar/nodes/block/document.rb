2133b9ceb83ce01e31fc32313f21f4a4d6a671b0
Nikolai Weibull <now@bitwi.se>
2011-07-27 14:17:36 +0200

    Refactor block handling in document

--- lib/nml/grammar/nodes/block/document.rb
+++ lib/nml/grammar/nodes/block/document.rb
@@ -3,7 +3,7 @@
 class NML::Grammar::Nodes::Block::Document < Treetop::Runtime::SyntaxNode
   def to_ast
     NML::AST::Document.new(*([title] +
-                            blocks.elements.map{ |e| e.block } +
+                            blocks.elements.map{ |e| e.block1 } +
                             sections.elements.map{ |e| e.section1 }).map{ |e| e.to_ast })
   end
 end
