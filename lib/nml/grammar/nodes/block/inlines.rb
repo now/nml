b74f7d49bc9e69c4b4ea8ea78c4dc9d0e521bc9b
Nikolai Weibull <now@bitwi.se>
2011-08-03 11:17:44 +0200

    Simplify item and inlines slightly

--- lib/nml/grammar/nodes/block/inlines.rb
+++ lib/nml/grammar/nodes/block/inlines.rb
@@ -2,10 +2,9 @@
 
 class NML::Grammar::Nodes::Block::Inlines < Treetop::Runtime::SyntaxNode
   def to_ast
-    ([first] + rest.elements.map{ |e| e.inlines }).
-      map{ |e| e.to_ast }.
+    ([first.to_ast] + rest.elements.map{ |e| e.inlines.to_ast }).
       flatten.
       reduce([]){ |a, e| String === e && String === a.last ? a.last << ' ' << e : a << e; a }.
-      map{ |e| String === e ? e.gsub(/[ \n\t]+/, ' ') : e }
+      map{ |e| String === e ? e.gsub(/\s+/, ' ') : e }
   end
 end
--- lib/nml/grammar/nodes/block/item.rb
+++ lib/nml/grammar/nodes/block/item.rb
@@ -1,9 +1,8 @@
 # -*- coding: utf-8 -*-
 
-class NML::Grammar::Nodes::Block::Item < Treetop::Runtime::SyntaxNode
+module NML::Grammar::Nodes::Block::Item
   def to_ast
-    NML::AST::Item.new(*([first] +
-                         (rest.elements || []).map{ |e| e.block }).
-                         map{ |e| e.to_ast })
+    NML::AST::Item.new(*([first.to_ast] +
+                         (rest.empty? ? [] : rest.to_ast)))
   end
 end
