c4ebf47e0c9759cf27008b570e4563232e27acb2
Nikolai Weibull <now@bitwi.se>
2011-08-24 16:11:43 +0200

    Allow empty lines inside code blocks
    This joins up code blocks that lay next to each other, but that’s better
    than having copy&paste not work.

--- lib/nml/grammar/nodes/block/code.rb
+++ lib/nml/grammar/nodes/block/code.rb
@@ -2,6 +2,6 @@
 
 class NML::Grammar::Nodes::Block::Code < Treetop::Runtime::SyntaxNode
   def to_ast
-    NML::AST::Block::Code.new(([first.text_value] + rest.elements.map{ |e| e.line.text_value }).join("\n"))
+    NML::AST::Block::Code.new(([first.text_value] + rest.elements.map{ |e| e.newlines.text_value + e.line.text_value }).join(''))
   end
 end
--- test/unit/nml/grammar/parsers/block/code2.rb
+++ test/unit/nml/grammar/parsers/block/code2.rb
@@ -8,4 +8,8 @@ Expectations do
   expect NML::AST::Block::Code.new("def id(a)\n  a\nend") do
     NML::Grammar::Parsers::Block::Code2Parser.ast("      def id(a)\n        a\n      end")
   end
+
+  expect NML::AST::Block::Code.new("def id(a)\n\n  a\n\nend") do
+    NML::Grammar::Parsers::Block::Code2Parser.ast("      def id(a)\n\n        a\n\n      end")
+  end
 end
