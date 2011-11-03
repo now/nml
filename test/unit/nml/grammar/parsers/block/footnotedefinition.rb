188199f32ba9ccc26aa482aa430677e4429137ed
Nikolai Weibull <now@bitwi.se>
2011-11-03 19:40:42 +0100

    Fix definition element

--- test/unit/nml/grammar/parsers/block/footnotedefinition.rb
+++ test/unit/nml/grammar/parsers/block/footnotedefinition.rb
@@ -6,6 +6,10 @@ Expectations do
     NML::Grammar::Parsers::Block::FootnoteDefinitionParser.ast('Email me at mailto:example@example.com')
   end
 
+  expect NML::AST::Block::Footnote::Link.
+           new('Email me', 'mailto:example@example.com') do
+    NML::Grammar::Parsers::Block::FootnoteDefinitionParser.ast("Email me at\n  mailto:example@example.com")
+  end
   expect NML::AST::Block::Footnote::Abbreviation.new('Neat Markup Language') do
     NML::Grammar::Parsers::Block::FootnoteDefinitionParser.ast('Abbreviation for Neat Markup Language')
   end
